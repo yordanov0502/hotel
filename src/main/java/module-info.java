33b172f7c9bd5c0b153befd41498036686b1a0b6
yorda <yordanovtodor281@gmail.com>
2022-10-16 13:18:01 +0300

    Initial commit + new JavaFx Project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module bg.tu_varna.sit.hotel {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens bg.tu_varna.sit.hotel to javafx.fxml;
+    exports bg.tu_varna.sit.hotel;
+}
