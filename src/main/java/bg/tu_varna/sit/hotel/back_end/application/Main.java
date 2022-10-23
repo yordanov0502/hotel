67a169baea8f92176339fb8bf867e0ea69a2c04a
Zdravkowe <zdravi.b0xer123@gmail.com>
2022-10-23 18:44:41 +0300

    version 1.0.0 (welcome + login pages)

--- src/main/java/bg/tu_varna/sit/hotel/back_end/application/Main.java
+++ src/main/java/bg/tu_varna/sit/hotel/back_end/application/Main.java
@@ -12,7 +12,7 @@ import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.util.Objects;
-
+//
 public class Main extends Application {
     public static Stage stage;
     public static double x, y;
